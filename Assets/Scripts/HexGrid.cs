f2c9b454a963faaa46c2f5c5c12429ebb7141142
LiefTheBeef <32721304+LiefTheBeef@users.noreply.github.com>
2020-07-02 16:07:01 -0700

    Hex Cells are now individually rendered

--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class HexGrid : MonoBehaviour
 {
+    // Variables //
     public int width;
-
     public int height;
 
     public HexCell cellPrefab;
-
     public HexCell[] cells;
-
     private HexMesh hexMesh;
-    
+
+    public Color defaultColor = Color.white;
+    public Color touchedColor = Color.magenta;
+
+
+    // Unity Functions
     private void Awake()
     {
         hexMesh = GetComponentInChildren<HexMesh>();
@@ -41,10 +44,11 @@ public class HexGrid : MonoBehaviour
         }
     }
 
+
+    // Functions //
     void CreateCell(int x, int z)
     {
         Vector3 position;
-        
         position.x = (x + z * 0.5f - z / 2) * (HexMetrics.innerRadius * 2f);
         position.y = 0f;
         position.z = z *  (HexMetrics.outerRadius * 1.5f);
@@ -53,22 +57,27 @@ public class HexGrid : MonoBehaviour
         cell.transform.SetParent(transform, false);
         cell.transform.localPosition = position;
         cell.coordinates = HexCoordinates.FromOffsetCoordinates(x,z);
+        cell.color = defaultColor;
     }
 
     void HandleInput()
     {
-        
         Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         if (Physics.Raycast(inputRay, out hit)) {
             TouchCell(hit.point);
         }
-   
     }
     
     void TouchCell (Vector3 position) {
         position = transform.InverseTransformPoint(position);
         HexCoordinates coordinates = HexCoordinates.FromPosition(position);
         Debug.Log("touched at " + coordinates.ToString());
+        
+        //Find index of the cell in the array, change its color, and re-triangulate it
+        int index = coordinates.X + coordinates.Z * width + coordinates.Z / 2;
+		HexCell cell = cells[index];
+		cell.color = touchedColor;
+		hexMesh.Triangulate(cells);
     }
 }
--- Assets/Scripts/HexMesh.cs
+++ Assets/Scripts/HexMesh.cs
@@ -5,36 +5,43 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class HexMesh : MonoBehaviour
 {
-
+    // Variables //
     Mesh hexMesh;
     List<Vector3> vertices;
     List<int> triangles;
+    List<Color> colors;
     MeshCollider meshCollider;
 
+
+    // Unity Functions //
     void Awake () {
         GetComponent<MeshFilter>().mesh = hexMesh = new Mesh();
         hexMesh.name = "Hex Mesh";
         vertices = new List<Vector3>();
         triangles = new List<int>();
+        colors = new List<Color>();
         meshCollider = gameObject.AddComponent<MeshCollider>();
-
     }
 
+
+    // Public Functions //
     public void Triangulate(HexCell[] cells)
     {
         hexMesh.Clear();
         vertices.Clear();
         triangles.Clear();
+        colors.Clear();
 
         for (int i = 0; i < cells.Length; i++)
         {
             Triangulate(cells[i]);
         }
+
         hexMesh.vertices = vertices.ToArray();
         hexMesh.triangles = triangles.ToArray();
+        hexMesh.colors = colors.ToArray();
         hexMesh.RecalculateNormals();
         meshCollider.sharedMesh = hexMesh;
-
     }
     
     
@@ -46,9 +53,10 @@ public class HexMesh : MonoBehaviour
                 center,
                 center + HexMetrics.corners[i],
                 center + HexMetrics.corners[i+1]
-            ); 
+            );
+            AddTriangleColor(cell.color);
         }
-
+        cell.gameObject.AddComponent<MeshFilter>();
     }
     
     void AddTriangle (Vector3 v1, Vector3 v2, Vector3 v3) {
@@ -60,5 +68,12 @@ public class HexMesh : MonoBehaviour
         triangles.Add(vertexIndex + 1);
         triangles.Add(vertexIndex + 2);
     }
-    
+
+
+    // Private/Helper Functions //
+    private void AddTriangleColor (Color color) {
+        colors.Add(color);
+        colors.Add(color);
+        colors.Add(color);
+    }
 }
